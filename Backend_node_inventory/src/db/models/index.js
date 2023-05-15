a88ee30ec329780b92e369c3e42e2f832471e7aa
Juan José Morales <jjmoralesz@correo.iue.edu.co>
2023-05-14 23:20:56 -0500

    Revert "Initial commit node_inventory"
    This reverts commit af46123c675c4565c168add0f6a5faf0bdcb5f22.

--- Backend_node_inventory/src/db/models/index.js
+++
@@ -1,7 +0,0 @@
-const { Product, ProductSchema } = require('./product.model');
-
-function setupModels(sequelize){
-    Product.init(ProductSchema, Product.config(sequelize));
-}
-
-module.exports =  setupModels ;
