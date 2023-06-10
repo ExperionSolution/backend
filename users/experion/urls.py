448ca93144e5dd19b71191ab88c94775e2a2399e
leonfio1515 <106787709+leonfio1515@users.noreply.github.com>
2023-06-10 12:24:27 -0300

    feat/Users
    added password recovery.

--- users/experion/urls.py
+++ users/experion/urls.py
@@ -11,7 +11,7 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
     path('sesion_group/<int:pk>/', SesionGroupView.as_view(), name='sesion_group'),
 
@@ -22,6 +22,13 @@ urlpatterns = [
     path('password_edit/<int:pk>/', PasswordEdit.as_view(), name='password_edit'),
     path('user_activate/<str:uidb64>/<str:token>/',UserActivate.as_view(), name='user_activate'),
 
+    path('reset_password/', PasswordRecoveryView.as_view(), name='reset_password'),
+    path('reset_password_sent/', PasswordRecoveryDone.as_view(),
+         name='reset_password_sent'),
+    path('reset/<uidb64>/<token>/', PasswordRecoveryConfirm.as_view(),
+         name='password_reset_confirm'),
+    path('reset_password_complete/', PasswordRecoveryComplete.as_view(),
+         name='reset_password_complete'),
 ]
 
 
--- users/experion/views.py
+++ users/experion/views.py
@@ -1,21 +1,33 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect, render, HttpResponseRedirect
+
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView, PasswordResetView
 from django.contrib.auth.forms import PasswordChangeForm
-from django.views.generic import TemplateView, CreateView, UpdateView, FormView, View
-from django.utils.text import capfirst
-from django.urls import reverse
 from django.contrib.auth.models import Group
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms.utils import ErrorList
 from django.contrib.auth import update_session_auth_hash
-
-
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth import login as auth_login
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.tokens import default_token_generator
+
+from django.urls import reverse, reverse_lazy
+
+from django.utils.text import capfirst
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.decorators import method_decorator
+from django.utils.translation import gettext_lazy as _
+
+from django.views.generic import TemplateView, CreateView, UpdateView, FormView, View
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+
+from django.forms.utils import ErrorList
+
+from django.core.exceptions import ImproperlyConfigured
+from django.core.mail import send_mail
+
+from django.template.loader import render_to_string
+
 from django.conf import settings
 
 from DB.models import *
@@ -23,6 +35,7 @@ from .forms import *
 ########################################################################
 
 
+#--------------------LOGIN-------------------------#
 class LoginView(LoginView):
     template_name = 'login.html'        
 
@@ -47,7 +60,7 @@ class SesionGroupView(View):
         return redirect('inicio')
     
 
-
+#--------------------USER CREATION-------------------------#
 class UserCreate(CreateView):
     model = Users
     form_class = UserCreateForm
@@ -115,6 +128,7 @@ class UserActivate(TemplateView):
             return redirect('register')
     
 
+#--------------------USER DATA EDIT-------------------------#
 class UserEdit(LoginRequiredMixin, UpdateView):
     model = Users
     form_class = UserEditForm
@@ -181,5 +195,135 @@ class PasswordEdit(LoginRequiredMixin, FormView):
 
         return context
 
+
+
+#--------------------PASSWORD RECOVERY-------------------------#
+class PasswordRecoveryView(PasswordResetView):
+    template_name = 'password_recovery_form.html'
+    email_template_name = 'password_recovery_email.html'
+    success_url = '../reset_password_sent'
+
+    def form_valid(self, form):
+        try:
+            current_site = get_current_site(self.request)
+            email = self.request.POST['email']
+            usuario = Users.objects.get(email=email)
+
+            uid = urlsafe_base64_encode(force_bytes(usuario.pk))
+            token = default_token_generator.make_token(usuario)
+
+            reset_url = reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})
+            reset_link = f"http://{current_site.domain}{reset_url}"
+
+            subject = 'Recuperacion de contraseña'
+            message = render_to_string('password_recovery_email.html', {
+                'usuario': usuario,
+                'reset_link': reset_link,
+                'domain': current_site.domain,
+                'uid': uid,
+                'token': token,
+            })
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [usuario.email])        
+            return super().form_valid(form)
+        except:
+            return render(self.request,'password_recovery_form.html', {"error": "El email ingresado no se encuentra registrado"})
+
+
+class PasswordRecoveryConfirm(FormView):
+    form_class = UserRecoveryPass
+    post_reset_login = False
+    post_reset_login_backend = None
+    reset_url_token = "set-password"
+    success_url = reverse_lazy("reset_password_complete")
+    template_name = "password_recovery_confirm.html"
+    title = _("Enter new password")
+    token_generator = default_token_generator
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(never_cache)
+    def dispatch(self, *args, **kwargs):
+        if "uidb64" not in kwargs or "token" not in kwargs:
+            raise ImproperlyConfigured(
+                "The URL path must contain 'uidb64' and 'token' parameters."
+            )
+
+        self.validlink = False
+        self.user = self.get_user(kwargs["uidb64"])
+
+        if self.user is not None:
+            token = kwargs["token"]
+            if token == self.reset_url_token:
+                session_token = self.request.session.get('reset_token')
+                if self.token_generator.check_token(self.user, session_token):
+                    self.validlink = True
+                    return super().dispatch(*args, **kwargs)
+            else:
+                if self.token_generator.check_token(self.user, token):
+                    self.request.session['reset_token'] = token
+                    redirect_url = self.request.path.replace(
+                        token, self.reset_url_token
+                    )
+                    return HttpResponseRedirect(redirect_url)
+
+        return self.render_to_response(self.get_context_data())
+
+    def get_user(self, uidb64):
+        UserModel = Users
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = UserModel._default_manager.get(pk=uid)
+        except (
+            TypeError,
+            ValueError,
+            OverflowError,
+            UserModel.DoesNotExist,
+            ValidationError,
+        ):
+            user = None
+        return user
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.user
+        return kwargs
+
+    def form_valid(self, form):
+        user = form.save()
+        del self.request.session['reset_token']
+        if self.post_reset_login:
+            auth_login(self.request, user, self.post_reset_login_backend)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.validlink:
+            context["validlink"] = True
+        else:
+            context.update(
+                {
+                    "form": None,
+                    "title": _("Password reset unsuccessful"),
+                    "validlink": False,
+                }
+            )
+        return context
+
+class PasswordRecoveryDone(PasswordResetDoneView):
+    template_name = 'password_recovery_done.html'
+
+
+class PasswordRecoveryComplete(TemplateView):
+    template_name = "password_recovery_complete.html"
+    title = _("Password reset complete")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["login_url"] = redirect(settings.LOGIN_URL)
+        return context
+
+
+#--------------------INDEX-------------------------#
 class Index(TemplateView):
     template_name = "index.html"
+
+
